dd6bebd442e350a8d4153644ad0f45d0589d9f8b
leeeha <jxlhe46@seoultech.ac.kr>
2021-11-06 13:19:55 +0900

    Week5 SearchScreen 복습

--- app/src/main/java/com/gdsc/cheggprepreview/screens/SearchScreen.kt
+++ app/src/main/java/com/gdsc/cheggprepreview/screens/SearchScreen.kt
@@ -5,22 +5,76 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Computer
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.gdsc.cheggprepreview.ui.theme.DeepOrange
 
+enum class SearchState {
+    ButtonScreen,
+    QueryScreen,
+    ResultScreen
+}
+
 @Composable
 fun SearchScreen(navController: NavHostController) {
+    val (screenState, setScreenState) = remember {
+        mutableStateOf(SearchState.ButtonScreen)
+    }
+
+    val (queryString, setQueryString) = remember {
+        mutableStateOf("")
+    }
+
+    // screenState에 따라 다른 화면 보여주기
+    when (screenState) {
+        SearchState.ButtonScreen -> {
+            SearchButtonScreen {
+                // 검색창이 비어 있지 않으면 ResultScreen
+                if(queryString.isNotBlank()){
+                    setScreenState(SearchState.ResultScreen)
+                }else{ // 검색창이 비어있으면 QueryScreen
+                    setScreenState(SearchState.QueryScreen)
+                }
+            }
+        }
+
+        SearchState.QueryScreen -> {
+            SearchQueryScreen(
+                queryString = queryString,
+                setQueryString = setQueryString,
+                toButtonScreen = { setScreenState(SearchState.ButtonScreen) },
+                toResultScreen = { setScreenState(SearchState.ResultScreen) }
+            )
+        }
+
+        SearchState.ResultScreen -> {
+            SearchResultScreen(
+                queryString = queryString,
+                setQueryString = setQueryString,
+                toButtonScreen = { setScreenState(SearchState.ButtonScreen) },
+                onSearchKey = { /* 검색 결과 업데이트 */ }
+            )
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -30,7 +84,7 @@ fun SearchScreen(navController: NavHostController) {
                     Row(
                         horizontalArrangement = Arrangement.Center,
                         modifier = Modifier.fillMaxWidth()
-                    ){
+                    ) {
                         Text(
                             text = "Find flashcards",
                             style = MaterialTheme.typography.h5,
@@ -45,9 +99,10 @@ fun SearchScreen(navController: NavHostController) {
             Modifier
                 .fillMaxSize()
                 .padding(vertical = 8.dp, horizontal = 16.dp)
-        ){
-            FindFlashCards(onClick = { /* 텍스트 입력 창이 나오게 해야 한다!! */ })
+        ) {
+            FindFlashCards(onClick = { /* 텍스트 입력 창이 나오도록 */ })
             Spacer(modifier = Modifier.height(24.dp))
+
             Divider(
                 Modifier
                     .fillMaxWidth(.15f)
@@ -55,22 +110,149 @@ fun SearchScreen(navController: NavHostController) {
                 color = DeepOrange
             )
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Choose your subject",
+
+            Text(
+                text = "Choose your subject",
                 style = MaterialTheme.typography.h6,
                 fontWeight = FontWeight.Bold
             )
             Spacer(modifier = Modifier.height(8.dp))
+
             Text(
                 text = "Jump into studying with free flashcards that are right for you",
                 style = MaterialTheme.typography.h6,
             )
             Spacer(modifier = Modifier.height(16.dp))
-            repeat(7){
+
+            // SubjectItem 7번 보여주기
+            repeat(7) {
                 SubjectItem()
                 Spacer(modifier = Modifier.height(8.dp))
             }
         }
     }
+}
+
+@Composable
+fun SearchButtonScreen(function: () -> Unit) {
+    TODO("Not yet implemented")
+}
+
+@Composable
+fun SearchQueryScreen(
+    queryString: String,
+    setQueryString: (String) -> Unit,
+    toButtonScreen: () -> Unit,
+    toResultScreen: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            SearchTopBar(
+                queryString = queryString,
+                setQueryString = setQueryString,
+                onBackButtonClick = toButtonScreen,
+                onSearchKey = toResultScreen
+            )
+        }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(.25f),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Center
+        ){
+            Text(
+                text = "what are you learning today?",
+                style = MaterialTheme.typography.body1,
+                fontSize = 20.sp,
+                color = Color.LightGray,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
+@Composable
+fun SearchResultScreen(
+    queryString: String,
+    setQueryString: (String) -> Unit,
+    toButtonScreen: () -> Unit,
+    onSearchKey: () -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(.25f),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.Center
+    ){
+        Text(
+            text = "$queryString 검색 결과",
+            style = MaterialTheme.typography.body1,
+            fontSize = 20.sp,
+            color = Color.LightGray,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun SearchTopBar(
+    queryString: String,
+    setQueryString: (String) -> Unit,
+    onBackButtonClick: () -> Unit,
+    onSearchKey: () -> Unit
+) {
+    TopAppBar(
+        elevation = 0.dp,
+        backgroundColor = Color.White,
+        // TopAppBar에 navigationIcon, title, actions 배치
+        navigationIcon = {
+            IconButton(onClick = onBackButtonClick) {
+                Icon(
+                    imageVector = Icons.Outlined.ArrowBack,
+                    contentDescription = "navigate back"
+                )
+            }
+        },
+        title = {
+            TextField(
+                value = queryString,
+                onValueChange = setQueryString,
+                placeholder = {
+                    Text(
+                        text = "Find flashcards",
+                        style = MaterialTheme.typography.h6,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Gray
+                    )
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    cursorColor = DeepOrange,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+
+                ),
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Search
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        onSearchKey()
+                    }
+                ),
+            )
+        },
+        actions = {
+            if (queryString.isNotBlank()) {
+                IconButton(onClick = { setQueryString("") }) {
+                    Icon(imageVector = Icons.Default.Close, contentDescription = "delete")
+                }
+            }
+        }
+    )
 }
 
 @Composable
